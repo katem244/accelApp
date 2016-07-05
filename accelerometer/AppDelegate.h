8a932b46956a169093413247e39749682ccfa84c
katem244 <kat@dhcp-ccc-5905.redrover.cornell.edu>
2016-07-05 17:54:08 -0400

    reboot repo

---
+++ accelerometer/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  accelerometer
+//
+//  Created by Katerina on 6/23/16.
+//  Copyright © 2016 Katerina. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+
+@end
